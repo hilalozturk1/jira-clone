39f2eb90478437c4f84c77bd6e27110807e5cdb6
hilalozturk1 <ozhilal222@gmail.com>
2024-11-07 00:38:04 +0300

    added card components in sign up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,10 +1,93 @@
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import { 
+  Card, 
+  CardContent, 
+  CardDescription, 
+  CardHeader, 
+  CardTitle 
+} from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 function SignUpPage() {
   return (
-    <div>
-        Sign up page
-    </div>
+    <Card className='w-full h-full md:w-[487px] border-none shadow-none'>
+      <CardHeader className='flex ştems-center justify-center text-center p-5'>
+        <CardTitle className='text-xl md:text-2xl'>
+          Sign Up!
+        </CardTitle>
+        <CardDescription>
+          By signin up, you agree to our {" "}
+          <Link href="/privacy">
+            <span className="text-blue-700">Privacy Policy</span>
+          </Link>{" "}and{" "}
+          <Link href="/terms">
+            <span className="text-blue-700">Terms of Service</span>
+          </Link>
+        </CardDescription>
+      </CardHeader>  
+      <div className='px-7 mb-2'>
+        <Separator/>
+      </div>
+      <CardContent className='py-7'>
+        <form className='space-y-4'>
+        <Input 
+            required
+            type='text'
+            value={""}
+            onChange={() => {}}
+            placeholder='Enter your name'
+            disabled={false}
+          />
+          <Input 
+            required
+            type='email'
+            value={""}
+            onChange={() => {}}
+            placeholder='Enter email address'
+            disabled={false}
+          />
+          <Input 
+            required
+            type='password'
+            value={""}
+            onChange={() => {}}
+            placeholder='Enter password'
+            disabled={false}
+            min={8}
+            max={256}
+          />
+          <Button className='w-full' size="lg" disabled={false}>Login</Button>
+        </form>
+        <div className="py-7">
+            <Separator/>
+          </div>
+        <Button 
+          disabled={false} 
+          variant="secondary" 
+          size="lg" 
+          className='w-full mb-2'
+        >
+          <FcGoogle className="mr-2 size-5"></FcGoogle>
+            Login with Google
+          </Button>
+        <Button 
+          disabled={false} 
+          variant="secondary" 
+          size="lg" 
+          className='w-full'
+          >
+          <FaGithub className="mr-2 size-5"></FaGithub>
+            Login with Github
+          </Button>
+      </CardContent>
+    </Card>
   )
 }
 
