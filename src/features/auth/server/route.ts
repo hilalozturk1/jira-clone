7fa2ed2ee4bb957ff96ccd72e74d739546beda86
hilalozturk1 <ozhilal222@gmail.com>
2024-11-08 12:51:50 +0300

    set session secret

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,14 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
+import { setCookie } from "hono/cookie";
 
 import { loginSchema, registerSchema } from "../schemas";
+import { AUTH_COOKIE } from "../constants";
+
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
 
 const app = new Hono()
     .post(
         "/login",
         zValidator("json", loginSchema),
         async (c) => {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const { email, password } = c.req.valid("json");
             return c.json({ success: "ok"})
         }
@@ -17,9 +22,30 @@ const app = new Hono()
         "/register",
         zValidator("json", registerSchema),
         async (c) => {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const { name, email, password } = c.req.valid("json");
-            return c.json({ success: "ok"})
+            
+            const { account } = await createAdminClient();
+            const user = await account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
+
+            const session = await account.createEmailPasswordSession(
+                email,
+                password
+            );
+
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30
+            });
+ 
+            return c.json({ success: "ok" });
         }
     )
 
