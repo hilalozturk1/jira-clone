04eff633d7fc91922cc76bcbe6d7657f648c5682
hilalozturk1 <ozhilal222@gmail.com>
2024-11-09 12:49:09 +0300

    unnecessary

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,16 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
-import { InferRequestType, InferResponseType } from "hono";
+import { InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
 
 type ResonseType = InferResponseType<typeof client.api.auth.logout["$post"]>;
-type RequestType = InferRequestType<typeof client.api.auth.logout["$post"]>;
 
 export const useLogin = () => {
     const mutation = useMutation<
     ResonseType,
-    Error,
-    RequestType>({
+    Error>({
         mutationFn: async() => {
            const response = await client.api.auth.logout.$post();
            return await response.json(); 
