df4fbe3425f27ea33b6daff9c4744e10d16af34f
hilalozturk1 <ozhilal222@gmail.com>
2024-11-09 14:10:28 +0300

    routing login page when logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useCurrent } from "@/features/auth/api/use-current";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const router = useRouter();
+  const { mutate } = useLogout();
+  const { data, isLoading } = useCurrent();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data])
+
   return (
     <div>
-      <Button>Click Me</Button>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
