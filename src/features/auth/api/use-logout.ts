eeeddfbd3d9680435f4eed0e9265d5ee967ebd6b
hilalozturk1 <ozhilal222@gmail.com>
2024-11-09 12:51:21 +0300

    success case

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
@@ -6,12 +6,17 @@ import { client } from "@/lib/rpc";
 type ResonseType = InferResponseType<typeof client.api.auth.logout["$post"]>;
 
 export const useLogin = () => {
+    const queryClient = useQueryClient();
+
     const mutation = useMutation<
     ResonseType,
     Error>({
         mutationFn: async() => {
            const response = await client.api.auth.logout.$post();
            return await response.json(); 
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["current"] })
         }
     });
 
