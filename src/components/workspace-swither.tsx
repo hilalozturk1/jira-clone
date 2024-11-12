30a3b5a78a16eeccc5eb91b948739c4104bc95d7
hilalozturk1 <ozhilal222@gmail.com>
2024-11-13 00:50:52 +0300

    routing workspace by workspace switcher

--- src/components/workspace-swither.tsx
+++ src/components/workspace-swither.tsx
@@ -2,6 +2,8 @@
 
 import { RiAddCircleFill } from "react-icons/ri";
 
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 
 import {
@@ -14,7 +16,13 @@ import {
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
 
 function WorkspaceSwither() {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
   const { data } = useGetWorkspaces();
+
+  const onSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  };
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -22,7 +30,7 @@ function WorkspaceSwither() {
         <p className="text-xs uppercase text-neutral-500">Workspaces</p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
           <SelectValue placeholder="No workspace selected." />
         </SelectTrigger>
