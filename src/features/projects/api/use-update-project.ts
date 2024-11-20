19059213f89c816b38bf288143ba9b3a5681b2fe
hilalozturk1 <ozhilal222@gmail.com>
2024-11-20 12:45:46 +0300

    update project

---
+++ src/features/projects/api/use-update-project.ts
@@ -0,0 +1,47 @@
+import { toast } from "sonner";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
+
+type ResonseType = InferResponseType<
+  (typeof client.api.projects)[":projectId"]["$patch"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.projects)[":projectId"]["$patch"]
+>;
+
+export const useUpdateProject = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResonseType, Error, RequestType>({
+    mutationFn: async ({ form, param }) => {
+      const response = await client.api.projects[":projectId"].$patch({
+        form,
+        param,
+      });
+
+      if (!response.ok) {
+        throw new Error("");
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success("Project creaupdatedted");
+      router.refresh();
+
+      queryClient.invalidateQueries({ queryKey: ["projects"] });
+      queryClient.invalidateQueries({ queryKey: ["project", data.$id] });
+    },
+    onError: () => {
+      toast.error("Failed to update project");
+    },
+  });
+
+  return mutation;
+};
--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -6,9 +6,10 @@ import { zValidator } from "@hono/zod-validator";
 
 import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config";
 
-import { createProjectSchema } from "../schema";
+import { createProjectSchema, updateProjectSchema } from "../schema";
 import { getMember } from "@/features/members/utils";
 import { sessionMiddleware } from "@/lib/session-middleware";
+import { Project } from "../types";
 
 const app = new Hono()
   .get(
@@ -100,6 +101,67 @@ const app = new Hono()
       );
 
       return c.json({ data: project });
+    }
+  )
+  .patch(
+    "/:projectId",
+    sessionMiddleware,
+    zValidator("form", updateProjectSchema),
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
+
+      const { projectId } = c.req.param();
+      const { name, image } = c.req.valid("form");
+
+      const project = await databases.getDocument<Project>(
+        DATABASE_ID,
+        PROJECTS_ID,
+        projectId
+      );
+
+      const member = await getMember({
+        databases,
+        workspaceId: project.workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`;
+      } else {
+        uploadedImageUrl = image;
+      }
+
+      const updatedProject = await databases.updateDocument(
+        DATABASE_ID,
+        PROJECTS_ID,
+        projectId,
+        {
+          name,
+          imageUrl: uploadedImageUrl,
+        }
+      );
+      return c.json({ data: updatedProject });
     }
   );
 
