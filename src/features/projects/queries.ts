63458b41441a496b802c8ba2fbd53a0f28d3a549
hilalozturk1 <ozhilal222@gmail.com>
2024-11-20 14:19:34 +0300

    fixed

--- src/features/projects/queries.ts
+++ src/features/projects/queries.ts
@@ -1,8 +1,8 @@
-import { Workspace } from "../workspaces/types";
 
 import { createSessionClient } from "@/lib/appwrite";
 import { DATABASE_ID, PROJECTS_ID } from "@/config";
 import { getMember } from "../members/utils";
+import { Project } from "./types";
 
 interface getProjectInfoProps {
   projectId: string;
@@ -13,7 +13,7 @@ export const getProjectInfo = async ({ projectId }: getProjectInfoProps) => {
   const { databases, account } = await createSessionClient();
   const user = await account.get();
 
-  const project = await databases.getDocument<Workspace>(
+  const project = await databases.getDocument<Project>(
     DATABASE_ID,
     PROJECTS_ID,
     projectId
