6835a6a60ed53cb32d1890865bd4b89514685464
hilalozturk1 <ozhilal222@gmail.com>
2024-11-20 18:08:33 +0300

    get project data

--- src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,11 +1,49 @@
-"use client";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-import { useProjectId } from "@/features/workspaces/hooks/use-project-id";
+import { PencilIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
 
-const Project = () => {
-  const projectId = useProjectId();
+import { getCurrent } from "@/features/auth/actions";
+import { getProjectInfo } from "@/features/projects/queries";
 
-  return <div>Project {projectId} </div>;
+interface ProjectIdPageProps {
+  params: {
+    projectId: string;
+  };
+}
+
+const Project = async ({ params }: ProjectIdPageProps) => {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
+  const initialValues = await getProjectInfo({
+    projectId: params.projectId,
+  });
+
+  return (
+    <div className="flex  max-w-screen-2xl mx-auto justify-between w-full p-[50px]">
+      <div className="flex items-center gap-x-2">
+        <WorkspaceAvatar
+          name={initialValues?.name}
+          image={initialValues?.imageUrl}
+          imageClassName="size-10"
+        />
+        <p className="text-md font-semibold p-2"> {initialValues?.name}</p>
+      </div>
+      <div>
+        <Button type="button" variant="secondary" size="sm" asChild>
+          <Link
+            href={`/workspaces/${initialValues?.workspaceId}/projects/${initialValues.$id}/settings`}
+          >
+            <PencilIcon />
+            Edit Button
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
