edb93851501a44feee7e128b63a7218265157014
hilalozturk1 <ozhilal222@gmail.com>
2024-11-20 17:47:26 +0300

    delete endpoin

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -163,6 +163,32 @@ const app = new Hono()
       );
       return c.json({ data: updatedProject });
     }
-  );
+  )
+  .delete("/:projectId", sessionMiddleware, async (c) => {
+    const databases = c.get("databases");
+    const user = c.get("user");
+
+    const { projectId } = c.req.param();
+
+    const project = await databases.getDocument<Project>(
+      DATABASE_ID,
+      PROJECTS_ID,
+      projectId
+    );
+
+    const member = await getMember({
+      databases,
+      workspaceId: project.workspaceId,
+      userId: user.$id,
+    });
+
+    if (!member) {
+      c.json({ error: "Unauthorized" });
+    }
+
+    await databases.deleteDocument(DATABASE_ID, PROJECTS_ID, projectId);
+
+    return c.json({ data: { $id: projectId } });
+  });
 
 export default app;
