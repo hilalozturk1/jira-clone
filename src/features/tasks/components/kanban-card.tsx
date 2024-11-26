c778062523c39b925a7d2ce720c0baaeaf16fab8
hilalozturk1 <ozhilal222@gmail.com>
2024-11-26 17:18:15 +0300

    task project info

--- src/features/tasks/components/kanban-card.tsx
+++ src/features/tasks/components/kanban-card.tsx
@@ -5,6 +5,7 @@ import { Task } from "../types";
 import { TaskDropdown } from "./task-dropdown";
 import { Separator } from "@/components/ui/separator";
 import { MemberAvatar } from "@/features/members/components/members-avatar";
+import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
 
 interface KanbanCardProps {
   task: Task;
@@ -29,6 +30,15 @@ export const KanbanCard = ({ task }: KanbanCardProps) => {
           {moment(task.dueDate).format("LL")}
         </div>
       </div>
+      <div className="flex items-center gap-x-3">
+          <WorkspaceAvatar
+          name={task.project.name}
+          image={task.project.image}
+          fallbackClassName="text-[10px] rounded-full"
+          className="text-sm size-5 "
+          />
+          <span className="text-sm">{task.project.name}</span>
+        </div>
     </div>
   );
 };
