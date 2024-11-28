12da387fe911f4e53c2a520089faa34644d00dca
hilalozturk1 <ozhilal222@gmail.com>
2024-11-28 23:40:46 +0300

    added task view to the dashboard

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/page.tsx
@@ -0,0 +1,14 @@
+import { redirect } from "next/navigation";
+
+import { getCurrent } from "@/features/auth/actions";
+
+import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher";
+
+const TasksPage = async () => {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
+  return <TaskViewSwitcher />;
+};
+
+export default TasksPage;
