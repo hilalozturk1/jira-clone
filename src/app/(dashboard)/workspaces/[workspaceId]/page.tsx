76bb540c9c75ce6347a4847a5313560f424be1fc
hilalozturk1 <ozhilal222@gmail.com>
2024-11-28 23:38:16 +0300

    fixed logout redirect

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,11 +1,12 @@
-"use client";
+import { redirect } from "next/navigation";
 
-function WorkspaceIdPage() {
-  return (
-    <div>
-      Workspace Id
-    </div>
-  )
+import { getCurrent } from "@/features/auth/actions";
+
+async function WorkspaceIdPage() {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
+  return <div>Workspace Id</div>;
 }
 
-export default WorkspaceIdPage
+export default WorkspaceIdPage;
