8c50d4b17069c1124d95dc230a5a1e4793019427
hilalozturk1 <ozhilal222@gmail.com>
2024-11-30 06:23:02 +0300

    get signle project

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -49,6 +49,34 @@ const app = new Hono()
       return c.json({ data: projects });
     }
   )
+  .get("/:projectId", sessionMiddleware, async (c) => {
+    const user = c.get("user");
+    const databases = c.get("databases");
+
+    const { projectId } = c.req.param();
+
+    const project = await databases.getDocument<Project>(
+      DATABASE_ID,
+      PROJECTS_ID,
+      projectId
+    );
+
+    if (!project.workspaceId) {
+      c.json({ error: "Missing workspaceId" }, 400);
+    }
+
+    const member = await getMember({
+      databases,
+      workspaceId: project.workspaceId,
+      userId: user.$id,
+    });
+
+    if (!member) {
+      c.json({ error: "Unauthorized" }, 401);
+    }
+
+    return c.json({ data: project });
+  })
   .post(
     "/",
     sessionMiddleware,
