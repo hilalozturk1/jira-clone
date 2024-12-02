4cdd7b82a353f07f3b4d19b1b3c16cfc107e18fe
hilalozturk1 <ozhilal222@gmail.com>
2024-12-02 03:37:58 +0300

    dynamic pathname

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,43 @@
+"use client";
+
 import { UserButton } from "@/features/auth/components/user-button";
 import MobileSidebar from "./mobile-sidebar";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+  const pathname = usePathname();
+  const [name, setName] = useState<string>("Home");
+
+  const dynamicName = (p: string) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    p.includes("projects") && p.includes("create")
+      ? setName("Create Project")
+      : p.includes("members")
+      ? setName("Member Setting")
+      : p.includes("settings")
+      ? setName("Workspace Setting")
+      : p.includes("tasks")
+      ? setName("Tasks")
+      : p.includes("projects")
+      ? setName("Project")
+      : p.includes("create")
+      ? setName("Create Workspace")
+      : setName("Home");
+  };
+
+  useEffect(() => {
+    dynamicName(pathname);
+  });
+
   return (
     <nav className="bg-slate-100 p-6 flex items-center justify-between">
       <div className="flex-col hidden lg:flex">
-        <h1 className="text-2xl text-slate-600 font-medium">Home</h1>
+        <h1 className="text-2xl text-slate-600 font-medium">{name}</h1>
         <p className="text-muted-foreground text-xs">
-          Monitor all of your projects and tasks here
+          {name === "Home" && (
+            <span>Monitor all of your projects and tasks here</span>
+          )}
         </p>
       </div>
       <MobileSidebar />
