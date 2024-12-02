b34a5095fbfdfb6fe29541ffa515a7fd09579778
hilalozturk1 <ozhilal222@gmail.com>
2024-12-03 01:52:38 +0300

    logout

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -2,22 +2,24 @@ import { useMutation } from "@tanstack/react-query";
 import { InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
-type ResponseType = InferResponseType<typeof client.api.auth.logout["$post"]>;
+type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
 
 export const useLogout = () => {
+  const router = useRouter();
 
-    const mutation = useMutation<
-    ResponseType,
-    Error>({
-        mutationFn: async() => {
-           const response = await client.api.auth.logout.$post();
-           return await response.json(); 
-        },
-        onSuccess: () => {
-           window.location.reload()
-        }
-    });
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.auth.logout.$post();
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.message("Succesfuly logout.");
+      router.push("/sign-in");
+    },
+  });
 
-    return mutation;
-}
+  return mutation;
+};
