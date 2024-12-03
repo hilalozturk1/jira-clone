c901c17174226cbd8494ffd07889ded2de5f15fd
hilalozturk1 <ozhilal222@gmail.com>
2024-12-04 00:46:19 +0300

    delete setCookie when registering

--- src/features/auth/api/use-register.ts
+++ src/features/auth/api/use-register.ts
@@ -2,11 +2,13 @@ import { useMutation } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.auth.register["$post"]>;
 type RequestType = InferRequestType<typeof client.api.auth.register["$post"]>;
 
 export const useRegister = () => {
+    const router = useRouter()
     const mutation = useMutation<
     ResponseType,
     Error,
@@ -14,6 +16,9 @@ export const useRegister = () => {
         mutationFn: async({ json }) => {
            const response = await client.api.auth.register.$post({ json });
            return await response.json(); 
+        },
+        onSuccess:() => {
+            router.push("/sign-in")
         }
     });
 
