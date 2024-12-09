9a1571f55c5b0e1964666372e5b2b94a6cbbc1c3
hilalozturk1 <ozhilal222@gmail.com>
2024-12-09 15:40:19 +0300

    fixed

--- src/app/(dashboard)/workspaces/create/page.tsx
+++ src/app/(dashboard)/workspaces/create/page.tsx
@@ -1,10 +1,7 @@
-import { redirect } from "next/navigation";
-import { getCurrent } from "@/features/auth/actions";
+
 import { CreateWorkSpaceForm } from "@/features/workspaces/components/create-workspace-form";
 
-async function workspaceCreatePage() {
-  const user = await getCurrent();
-  if (!user) redirect("/sign-in");
+ function workspaceCreatePage() {
 
   return (
     <div className="w-full lg:max-w-xl">
