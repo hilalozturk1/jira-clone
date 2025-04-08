4b7a0ee01b34733b66984df31291d9fd4e16249b
hilalozturk1 <ozhilal222@gmail.com>
2025-04-09 02:26:16 +0300

    messages

--- assets/responses.ts
+++ assets/responses.ts
@@ -7,17 +7,16 @@ type TResponse = {
 export const responses: Record<string, Record<string, TResponse>> = {
   login: {
     error: { status: 401, message: "Please check the email and password." },
-    success: { status: 200, message: "Success." },
   },
   projects: {
     error: { status: 401, message: "Failed to fetch projects." },
-    success: { status: 200, message: "Success." },
   },
   current: {
     error: { status: 401, message: "Do not found user." },
-    success: { status: 200, message: "Success." },
   },
   general: {
+    unauthorized: { status: 401, message: "Unauthorized" },
+    success: { status: 200, message: "Success." },
     notfound: { status: 404, message: "Not found" },
     badrequest: { status: 400, message: "Bad request" },
     zoderror: { status: 400, message: "Invalid type" },
